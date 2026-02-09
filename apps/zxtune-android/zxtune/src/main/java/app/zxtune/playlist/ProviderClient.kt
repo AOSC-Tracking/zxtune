1107e070e2a62c582aa33f4a2e9143c842301287
vitamin-caig <vitamin.caig@gmail.com>
2026-02-10 00:09:10 +0300

    zxtune-android: support multiple playlists in UI.

--- apps/zxtune-android/zxtune/src/main/java/app/zxtune/playlist/ProviderClient.kt
+++ apps/zxtune-android/zxtune/src/main/java/app/zxtune/playlist/ProviderClient.kt
@@ -7,6 +7,7 @@ import androidx.annotation.VisibleForTesting
 import app.zxtune.analytics.Analytics
 import app.zxtune.playlist.IO.asStatistics
 import app.zxtune.playlist.IO.toContentValues
+import app.zxtune.playlist.IO.toPlaylist
 import app.zxtune.playlist.IO.toTrack
 import app.zxtune.ui.utils.observeChanges
 import app.zxtune.ui.utils.query
@@ -21,21 +22,24 @@ class PlaylistContent(size: Int) : ArrayList<Track>(size)
 
 class ProviderClient @VisibleForTesting constructor(
     private val resolver: ContentResolver,
+    private val playlist: Playlist.Id,
     private val dispatcher: CoroutineDispatcher,
 ) {
-    fun add(track: Track.Metadata) = resolver.insert(PlaylistQuery.ALL, track.toContentValues())
+    private val playlistUri = Query.localPlaylistUri(playlist)
 
-    fun notifyChanges() = resolver.notifyChange(PlaylistQuery.ALL, null)
+    fun add(track: Track.Metadata) = resolver.insert(playlistUri, track.toContentValues())
+
+    fun notifyChanges() = resolver.notifyChange(playlistUri, null)
 
     @OptIn(FlowPreview::class)
-    fun observeContent() = resolver.observeChanges(PlaylistQuery.ALL).debounce(1000).transform {
+    fun observeContent() = resolver.observeChanges(playlistUri).debounce(1000).transform {
         queryContent()?.let {
             emit(it)
         }
     }
 
     @VisibleForTesting
-    suspend fun queryContent() = resolver.query(PlaylistQuery.ALL) { cursor ->
+    suspend fun queryContent() = resolver.query(playlistUri) { cursor ->
         PlaylistContent(cursor.count).apply {
             while (cursor.moveToNext()) {
                 add(cursor.toTrack())
@@ -48,24 +52,24 @@ class ProviderClient @VisibleForTesting constructor(
         Analytics.sendPlaylistEvent(Analytics.PlaylistAction.DELETE, tracks.size)
     }
 
-    suspend fun deleteAll() {
+    suspend fun clear() {
         deleteItems(null)
         Analytics.sendPlaylistEvent(Analytics.PlaylistAction.DELETE, 0)
     }
 
     private suspend fun deleteItems(tracks: Track.IdSet?) = withContext(dispatcher) {
-        Provider.delete(resolver, tracks)
+        Provider.delete(resolver, Playlist.OperationScope(playlist, tracks))
         notifyChanges()
     }
 
     suspend fun reorder(track: Track.Id, delta: Int) = withContext(dispatcher) {
-        Provider.reorder(resolver, track, delta)
+        Provider.reorder(resolver, Track.FullIdentifier(playlist, track), delta)
         notifyChanges()
         Analytics.sendEvent("ui/playlist/reorder", "delta" to delta)
     }
 
     suspend fun sort(spec: Playlist.Sorting) = withContext(dispatcher) {
-        Provider.sort(resolver, spec)
+        Provider.sort(resolver, playlist, spec)
         notifyChanges()
         Analytics.sendEvent(
             "ui/playlist/sort",
@@ -76,14 +80,47 @@ class ProviderClient @VisibleForTesting constructor(
 
     suspend fun statistics(tracks: Track.IdSet?) = withContext(dispatcher) {
         Analytics.sendPlaylistEvent(Analytics.PlaylistAction.STATISTICS, tracks?.size ?: 0)
-        Provider.statistics(resolver, tracks)?.asStatistics()
+        Provider.statistics(resolver, Playlist.OperationScope(playlist, tracks))?.asStatistics()
     }
+
+    @Throws(Exception::class)
+    suspend fun save(name: String, tracks: Track.IdSet?) = withContext(dispatcher) {
+        Provider.save(resolver, name, Playlist.OperationScope(playlist, tracks))
+        Analytics.sendPlaylistEvent(Analytics.PlaylistAction.SAVE, tracks?.size ?: 0)
+    }
+
+    companion object {
+        @JvmStatic
+        fun create(ctx: Context) = ProviderClient(ctx.contentResolver, Playlist.DEFAULT_ID, Dispatchers.IO)
+
+        fun createUri(id: Track.FullIdentifier): Uri = Query.itemUri(id)
+
+        fun findId(uri: Uri) = Query.findTrackId(uri)
+    }
+}
+
+class AggregatingProviderClient(private val resolver: ContentResolver, private val dispatcher: CoroutineDispatcher) {
+    @OptIn(FlowPreview::class)
+    fun observePlaylists() = resolver.observeChanges(Query.savedPlaylistsUri()).debounce(1000).transform {
+        queryPlaylists()?.let {
+            emit(it)
+        }
+    }
+
+    @VisibleForTesting
+    suspend fun queryPlaylists() = resolver.query(Query.savedPlaylistsUri()) { cursor ->
+        ArrayList<Playlist>(cursor.count).apply {
+            while (cursor.moveToNext()) {
+                add(cursor.toPlaylist())
+            }
+        }
+    }
+
+    fun getPlaylist(playlist: Playlist.Id) = ProviderClient(resolver, playlist, dispatcher)
 
     // id => path
     suspend fun getSavedPlaylists(id: String? = null) = withContext(dispatcher) {
-        resolver.query(
-            PlaylistQuery.SAVED, selection = id
-        ) { cursor ->
+        resolver.query(Query.savedPlaylistsUri(), selection = id) { cursor ->
             HashMap<String, String>().apply {
                 while (cursor.moveToNext()) {
                     put(cursor.getString(0), cursor.getString(1))
@@ -92,20 +129,7 @@ class ProviderClient @VisibleForTesting constructor(
         }
     }
 
-    @Throws(Exception::class)
-    suspend fun savePlaylist(name: String, tracks: Track.IdSet?) = withContext(dispatcher) {
-        Provider.save(resolver, name, tracks)
-        Analytics.sendPlaylistEvent(Analytics.PlaylistAction.SAVE, tracks?.size ?: 0)
-    }
-
     companion object {
-        @JvmStatic
-        fun create(ctx: Context) = ProviderClient(ctx.contentResolver, Dispatchers.IO)
-
-        fun createUri(id: Long): Uri = PlaylistQuery.uriFor(id)
-
-        @JvmStatic
-        fun findId(uri: Uri) =
-            if (PlaylistQuery.isPlaylistUri(uri)) PlaylistQuery.idOf(uri) else null
+        fun create(ctx: Context) = AggregatingProviderClient(ctx.contentResolver, Dispatchers.IO)
     }
 }
